15edf5e3f5728f1721f478b6ef0a7e450c3eb714
mithrilwoodrat <mithrilwoodrat@gmail.com>
2015-06-09 23:34:40 +0800

    add new UI screenshots

--- initdb.py
+++ initdb.py
@@ -9,14 +9,15 @@ from config import SQLALCHEMY_DATABASE_URI
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 Base = declarative_base()
 
+
 class Target(Base):
     __tablename__ = 'targets'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     host = Column(String)
+    plugin_name = Column(String)
     result = Column(String)
 
     def __repr__(self):
-        return "<Target(host='%s', result='%s')>" % (
-                             self.host,self.result)
+        return "<Target(host='%s', plugin_name='%s', result='%s')>" % (self.host, self.plugin_name, self.result)
 
 Base.metadata.create_all(engine)
