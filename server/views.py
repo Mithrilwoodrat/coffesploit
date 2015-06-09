e5f8ce4be770d3e1a14d99e94a990f6c8ae87f9a
mithrilwoodrat <mithrilwoodrat@gmail.com>
2015-06-09 23:31:37 +0800

    change to new UI

--- server/views.py
+++ server/views.py
@@ -2,7 +2,7 @@
 import os
 from server import csfserver
 from flask import render_template, flash, jsonify, request, redirect, url_for
-from config import UPLOAD_FOLDER,allowed_file
+from config import UPLOAD_FOLDER, allowed_file
 from werkzeug.utils import secure_filename
 from server import main
 
@@ -10,40 +10,23 @@ from server import main
 @csfserver.route('/')
 @csfserver.route('/index')
 def index():
-    return render_template("index.html", title="Welcome to Coffesploit")
+    return render_template("index.html")
 
-@csfserver.route('/base')
-def base():
-    return render_template("base.html")
+
+@csfserver.route('/new_scan')
+def new_scan():
+    return render_template("new_scan.html")
+
 
 @csfserver.route('/about')
 def about():
     return render_template("about.html")
 
 
-@csfserver.route('/config')
-def config():
-    plugin_list = main.plugin_list
-    scan_plugins = []
-    exploit_plugins = []
-    payload_plugins = []
-    for plugin in plugin_list:
-        if plugin_list[plugin][0] == "scan":  # type == scan
-            scan_plugins.append(plugin)
-        elif plugin_list[plugin][0] == "exploit":  # type == exploit
-            exploit_plugins.append(plugin)
-        elif plugin_list[plugin][0] == "payload":  # type == payload
-            payload_plugins.append(plugin)
-    flash("Click button to choose plugin")
-    return render_template("config.html", title="Choose Plugin",
-                           scan_plugins=scan_plugins,
-                           exploit_plugins=exploit_plugins,
-                           payload_plugins=payload_plugins,
-                           )
-
 @csfserver.route('/setting')
 def setting():
     return render_template('setting.html',title="Upload your plugin")
+
 
 @csfserver.route('/upload/<plugin_type>', methods=['POST', 'GET'])
 def upload(plugin_type):
@@ -63,29 +46,35 @@ def set_plugin():
     main.use(plugin_name)
     return jsonify(plugin=plugin_name)
 
+
 @csfserver.route('/plugin/<plugin_name>', methods=['GET', 'POST'])
 def plugin(plugin_name):
     if request.method == 'GET':
         main.use(plugin_name)
-    plugin_name = main.current_plugin_name()
-    plugin_type = main.current_plugin_type()
-    print "Using Plugin:", plugin_name
-    status = None
-    if main.pluginmanager.current_plugin is not None:
-        status = main.pluginmanager.plugin_status()
-    if status is None:
-        flash("error while parse status")
-    if request.method == 'POST':
-        for arg in status:
-            print arg, ":", request.form.get(arg)
-            plu_arg = request.form.get(arg)
-            plu_arg = str(plu_arg)
-            if plu_arg is not None and plu_arg != "":
-                main.set(str(arg), plu_arg)
-        return redirect(url_for('reports'))
-    return render_template("plugin.html", title=plugin_type+"/"+plugin_name,
-                           plugin_name=plugin_name,
-                           status=status)
+        plugin_name = main.current_plugin_name()
+        plugin_type = main.current_plugin_type()
+        print "Using Plugin:", plugin_name
+        status = None
+        if main.pluginmanager.current_plugin is not None:
+            status = main.pluginmanager.plugin_status()
+        if status is None:
+            return jsonify("ERROR!")
+        return jsonify(title=plugin_type+"/"+plugin_name,plugin_name=plugin_name,status=status)
+
+    #if request.method == 'POST':
+    #    status = None
+    #    if main.pluginmanager.current_plugin is not None:
+    #        status = main.pluginmanager.plugin_status()
+    #    if status is None:
+    #        return jsonify("ERROR!")
+    #    for arg in status:
+    #        print arg, ":", request.form.get(arg)
+    #        plu_arg = request.form.get(arg)
+    #        plu_arg = str(plu_arg)
+    #        if plu_arg is not None and plu_arg != "":
+    #            main.set(str(arg), plu_arg)
+    #    return jsonify(status='SUCCESS')
+
 
 @csfserver.route('/run_plugin', methods=['GET','POST'])
 def run_plugin():
@@ -97,8 +86,9 @@ def run_plugin():
             plu_arg = str(plu_arg)
             if plu_arg is not None and plu_arg != "":
                 main.set(str(arg), plu_arg)
-        main.pluginmanager.current_plugin.run()
-    return redirect(url_for('reports'))
+        main.pluginmanager.plugin_run()
+        return jsonify(status='SUCCESS')
+
 
 @csfserver.route('/reports')
 def reports():
@@ -111,7 +101,6 @@ def reports():
                            title=main.current_plugin_name(),
                            result=main.pluginmanager.plugin_result(),
                            )
-
 
 
 @csfserver.route('/plugin_list')
